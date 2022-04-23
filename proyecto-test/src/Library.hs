61e167c54ee5ceb38000212dafb8767db98f053d
Nicolas Sicolo <sicolo2208@gmail.com>
2022-04-23 18:37:58 -0300

    nuevas modificaciones ver tp

--- proyecto-test/src/Library.hs
+++ proyecto-test/src/Library.hs
@@ -16,3 +16,21 @@ cuadrado numero = numero * numero
 
 dobleDelCuadrado :: Number -> Number
 dobleDelCuadrado numero = doble (cuadrado numero)
+
+
+
+data Persona = Persona {
+    nombre :: String,
+    edad :: Number,
+    stress :: Number,
+    preferencias :: [String],
+    cantAmigues :: [String]
+}
+
+
+
+scoring :: Persona -> Number
+
+scoring persona | (even . length . cantAmigues)persona = stress persona * edad persona
+                | edad persona > 40                   = (length .cantAmigues) persona * edad persona
+                | otherwise                   = ((*2) . length . nombre)persona
--- proyecto-test/src/Spec.hs
+++ proyecto-test/src/Spec.hs
@@ -2,10 +2,48 @@ module Spec where
 import PdePreludat
 import Library
 import Test.Hspec
+import GHC.Num (Num)
 
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "Test de ejemplo" $ do
     it "El pdepreludat se instaló correctamente" $ do
       doble 1 `shouldBe` 2
+    it "Cantidad par de amigues, edad 25 y nivel de stress 10" $ do
+      scoring (Persona "Nico" 25 10 ["Juan"] ["Nico", "Juan"]) `shouldBe` 250
+
+
+
+
+
+
+
+
+
+
+
+    
+    -- it "Persona de 41 años y un amigue" $ do
+    --   scoring "Pepa" 41 0 [ ] ["juan"] `shouldBe` 41
+    
+    -- it "Persona de 31 años y un amigue, llamada Rigoberta" $ do
+    --   scoring "Rigoberta" 31 0 [ ] 1 `shouldBe` 18
+
+    -- it "Una persona que se llama Paulina" $ do
+    --   nombreFirme "Paulina" 1 2 "_" 3 `shouldBe` True
+    
+    -- it "Una persona que se llama Rigoberta" $ do
+    --   nombreFirme "Rigoberta" 1 2 "_" 3 `shouldBe` False
+
+    -- it "Una persona con 1 amigo" $ do
+    --   personaInteresante "A" 1 2 "_" 1 `shouldBe` False
+
+    -- it "Una persona con 2 amigos" $ do
+    --   personaInteresante "A" 1 2 "_" 2 `shouldBe` True
+
+    -- it "Una persona con 3 amigos" $ do
+    --   personaInteresante "A" 1 2 "_" 3 `shouldBe` True
+    
+
+
 
